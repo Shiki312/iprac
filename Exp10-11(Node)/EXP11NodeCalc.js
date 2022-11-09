0ca2ca064d44b86ac709bb32f5eda1166f81a26d
Shiki312 <114181280+Shiki312@users.noreply.github.com>
2022-11-09 17:58:55 +0530

    Add files via upload

---
+++ Exp10-11(Node)/EXP11NodeCalc.js
@@ -0,0 +1,11 @@
+// npm install basic-calculator-js
+
+const calculate = require("basic-calculator-js")
+ 
+var x = 50;
+var y = 50;
+ 
+console.log("Multiply: "+calculate.multiply(x,y));
+console.log("Subtract: "+calculate.subtract(x, y));
+console.log("Addition: "+calculate.add(x,y));
+console.log("Divide: "+calculate.divide(x, y));
